f18eb9ca66a121e3dc26b1986abe838a270a542a
Yanara Caballero Delgado <yanaracaballero97@gmail.com>
2023-10-12 11:57:48 +0200

    main slider v.0.1

--- schemas/imagenSchema.js
+++ schemas/imagenSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const imagenSchema = new mongoose.Schema(
-    { src : String , alt : String , active : String , static : String },
+    { src : String , alt : String , active : String , statico : String },
     { collection : 'imagenes' }
 )
 
